71ca08cf6692fa64ca7731e3fc4f350e9097473e
Jonahttp <jonahttp333@gmail.com>
2023-03-13 19:39:06 -0500

    fixed pages review

--- Jacked/client/src/pages/Home.js
+++ Jacked/client/src/pages/Home.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import { Link, Navigate } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import { Link, Navigate } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 
-import ReviewList from '../components/ReviewList';
-import ReviewForm from '../components/ReviewForm';
+import ReviewForm from "../components/ReviewForm";
 
-import { QUERY_THOUGHTS } from '../utils/queries';
+import { QUERY_THOUGHTS } from "../utils/queries";
 
 const Home = () => {
   // const { loading, data } = useQuery(QUERY_THOUGHTS);
@@ -16,17 +15,17 @@ const Home = () => {
       <div className="flex-row justify-center">
         <div
           className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
+          style={{ border: "1px dotted #1a1a1a" }}
         >
           <ReviewForm />
         </div>
         <div
           className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
+          style={{ border: "1px dotted #1a1a1a" }}
         >
-         Checkout Our Reviews <a href='/Reviews'>Here</a>
+          Checkout Our Reviews <a href="/reviews">Here</a>
         </div>
-        
+
         {/* <div className="col-12 col-md-8 mb-3">
           {loading ? (
             <div>Loading...</div>
--- Jacked/client/src/pages/Reviews.js
+++ Jacked/client/src/pages/Reviews.js
@@ -1,18 +1,43 @@
 import React from 'react';
+import { Link, Navigate } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import ReviewList from '../components/ReviewList';
+import ReviewForm from '../components/ReviewForm';
+
 import { QUERY_THOUGHTS } from '../utils/queries';
 
 const Reviews = () => {
   const { loading, data } = useQuery(QUERY_THOUGHTS);
   const thoughts = data?.thoughts || [];
-
   return (
     <main>
       <div className="flex-row justify-center">
+        <div
+          className="col-12 col-md-10 mb-3 p-3"
+          style={{ border: '1px dotted #1a1a1a' }}
+        >
+          <ReviewList
+              thoughts={thoughts}
+              title="Checkout Our Reviews..."
+            />
+        </div>
+        <div
+          className="col-12 col-md-10 mb-3 p-3"
+          style={{ border: '1px dotted #1a1a1a' }}
+        >
+         Checkout Our Reviews <a href='/Reviews'>Here</a>
+        </div>
+        
         <div className="col-12 col-md-8 mb-3">
-            <ReviewList/>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ReviewList
+              thoughts={thoughts}
+              title="Checkout Our Reviews..."
+            />
+          )}
         </div>
       </div>
     </main>
