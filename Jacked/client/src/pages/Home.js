c41be725246f78c5974c7b57632dcc098710f0bf
Jonahttp <jonahttp333@gmail.com>
2023-03-14 18:49:25 -0500

    removed gitignored files (node_modules_

--- Jacked/client/src/pages/Home.js
+++ Jacked/client/src/pages/Home.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { Link, Navigate } from "react-router-dom";
-import { useQuery } from "@apollo/client";
+import { useQuery } from '@apollo/client';
 
 import ReviewForm from "../components/ReviewForm";
 
-import { QUERY_THOUGHTS } from "../utils/queries";
+import { QUERY_THOUGHTS } from '../utils/queries';
+
+
+
 
 const Home = () => {
-  // const { loading, data } = useQuery(QUERY_THOUGHTS);
-  // const thoughts = data?.thoughts || [];
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const thoughts = data?.thoughts || [];
 
   return (
     <main>
@@ -17,6 +19,8 @@ const Home = () => {
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: "1px dotted #1a1a1a" }}
         >
+            {/* {loading ? (
+            <div>Loading...</div> */}
           <ReviewForm />
         </div>
         <div
@@ -25,17 +29,6 @@ const Home = () => {
         >
           Checkout Our Reviews <a href="/reviews">Here</a>
         </div>
-
-        {/* <div className="col-12 col-md-8 mb-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ReviewList
-              thoughts={thoughts}
-              title="Checkout Our Reviews..."
-            />
-          )}
-        </div> */}
       </div>
     </main>
   );
